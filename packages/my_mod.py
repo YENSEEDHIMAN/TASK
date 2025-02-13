7b9c23301c95364ee6d1a699ddc3bbdb9a7fcc92
Yensee <yensee@yensee-trainee.SEASIAINFOTECH.COM>
2025-02-13 17:57:29 +0530

    commiting my updates

---
+++ packages/my_mod.py
@@ -0,0 +1,13 @@
+def add(a,b):
+    print(a+b)
+def sub(a,b):
+    print(a-b)
+def mul(a,b):
+    print(a*b)
+def div(a,b):
+    print(a/b)
+
+def call(name):
+    return(f"Hello.... Your name is {name}")
+
+mob=9876543210
